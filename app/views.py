bb4ed7f1c7840ef7414c8ffc57ab2840cd373518
Melissa Longenberger <apple@Apples-iMac.hitronhub.home>
2021-11-08 23:27:13 -0500

    more base files

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    pass
+
+def allMoveies(request):
+    pass
+
+def theAdmin(request):
+    pass
+
+def register(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
